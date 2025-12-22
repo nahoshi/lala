c47acc2662cc098cc9ce053465613d4509a8ab53
Nahoshi <nahoshi@proton.me>
2025-12-22 19:21:00 -0300

    setup: Initialize BetterAuth.

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    advanced: {
+        database: {
+            generateId: false
+        }
+    },
+    plugins: [nextCookies()],
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: false
+    }
+})
