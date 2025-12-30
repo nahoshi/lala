41d388a1d5bcaa65671ae2aa95ee79faeb86662f
Nahoshi <nahoshi@proton.me>
2025-12-29 23:26:17 -0300

    feat: Add email sending support to better-auth.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 import { nextCookies } from "better-auth/next-js";
+import { resend } from "./server/resend";
+import ResetPasswordEmail from "@/components/emails/reset-password";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -16,6 +18,18 @@ export const auth = betterAuth({
   plugins: [nextCookies()],
   emailAndPassword: {
     enabled: true,
+    sendResetPassword: async ({ user, url }) => {
+      await resend.emails.send({
+        from: process.env.RESEND_EMAIL!,
+        to: user.email,
+        subject: "Alteração da sua senha",
+        react: ResetPasswordEmail({
+          name: user.name,
+          url: url.replace("/api/auth", ""),
+        }),
+      });
+    },
+    resetPasswordTokenExpiresIn: 60 * 60 * 3,
   },
   socialProviders: {
     spotify: {
